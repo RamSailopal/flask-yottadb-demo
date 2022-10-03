b9ddb66fb3e1e02bf2d4ff5d02388e86aea7aec4
Raman Sailopal <rsailopal@robinsonbrothers.co.uk>
2022-10-03 12:57:06 +0100

    Added initialisation of global

--- flask-back/entrypoint.sh
+++ flask-back/entrypoint.sh
@@ -3,6 +3,7 @@ apt-get update
 apt-get install -y python3 python3-pip python3-dev python3-setuptools libffi-dev 
 pip3 install flask mysql-connector-python
 source /opt/yottadb/current/ydb_env_set
+/opt/yottadb/current/ydb <<< 'S ^PATIENTS=""'
 pip install yottadb
 cd /home/flask-back
 export FLASK_APP=index
